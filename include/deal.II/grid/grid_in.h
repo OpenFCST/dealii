3e21b4c098b8fa79c01df8f4fd2a0d0b8ac76481
Aslan Kosakian <kosakian@ualberta.ca>
2020-07-10 11:26:32 -0600

    Revert "Merged with deal.ii/master"
    This reverts commit a4877c7b0785f7a2aebd2f9b9024b0f1ec9f55b5, reversing
    changes made to 86b6063efa7c1439f52a97d675dbe17c4dacaec2.

--- include/deal.II/grid/grid_in.h
+++ include/deal.II/grid/grid_in.h
@@ -669,26 +669,8 @@ public:
 
 
   DeclException0(ExcGmshNoCellInformation);
-  
-  /**
-   * Return a map containing field data. The format of the returned map is as follows: 
-   * - std::string stores the name of the field data (identifier) as specified in the external mesh
-   * - std::vector<double > stores value for the given identifier in each cell. To access the value use field_data[name_field][cell_id].
-   */
-  std::map< std::string, std::vector<double> >& get_field_data()
-  {      
-      return field_data;
-  }
 
 protected:
-    
-  /**
-  * Data member that stores cell data. The format is as follows:
-  * - std::string stores the name of the field data (identifier) as specified in the external mesh
-  * - std::vector<double> stores value for the given identifier in each cell_id. To access the value use field_data[name_field][cell_id].
-  */
-  std::map< std::string, std::vector<double> > field_data;
-  
   /**
    * Store address of the triangulation to be fed with the data read in.
    */
