OpenFCST/dealii

commit b1883d9cbf2dfa8452ebb109d580096453f7e10f

qualify namspace for MSVC
otherwise MSVC tries to use
numbers::internal from signaling_nan.h

include/deal.II/base/numbers.h
@@ -836,8 +836,8 @@ namespace numbers
   values_are_equal(const adouble &value_1, const Number &value_2)
   {
     // Use the specialized definition for two ADOL-C taped types
-    return values_are_equal(value_1,
-                            internal::NumberType<adouble>::value(value_2));
+    return values_are_equal(
+      value_1, dealii::internal::NumberType<adouble>::value(value_2));
   }
 
 
@@ -892,7 +892,7 @@ namespace numbers
   {
     // Use the specialized definition for two ADOL-C taped types
     return value_is_less_than(value_1,
-                              internal::NumberType<adouble>::value(value_2));
+                              ::internal::NumberType<adouble>::value(value_2));
   }
 
 
@@ -912,8 +912,8 @@ namespace numbers
   value_is_less_than(const Number &value_1, const adouble &value_2)
   {
     // Use the specialized definition for two ADOL-C taped types
-    return value_is_less_than(internal::NumberType<adouble>::value(value_1),
-                              value_2);
+    return value_is_less_than(
+      dealii::internal::NumberType<adouble>::value(value_1), value_2);
   }
 
 #endif
@@ -923,7 +923,7 @@ namespace numbers
   constexpr bool
   values_are_equal(const Number1 &value_1, const Number2 &value_2)
   {
-    return (value_1 == internal::NumberType<Number1>::value(value_2));
+    return (value_1 == dealii::internal::NumberType<Number1>::value(value_2));
   }
 
 
@@ -947,7 +947,7 @@ namespace numbers
   inline bool
   value_is_less_than(const Number1 &value_1, const Number2 &value_2)
   {
-    return (value_1 < internal::NumberType<Number1>::value(value_2));
+    return (value_1 < dealii::internal::NumberType<Number1>::value(value_2));
   }
 
 

commit d6a7376a404b72773436a607caebb84529deb67c

Fix a typo in deal.II/base/numbers.h
The refactoring done in b1883d9cbf2dfa8452ebb109d580096453f7e10f
introduced a typo that wasn't spotted by our CI. Fix this.

include/deal.II/base/numbers.h
@@ -891,8 +891,8 @@ namespace numbers
   value_is_less_than(const adouble &value_1, const Number &value_2)
   {
     // Use the specialized definition for two ADOL-C taped types
-    return value_is_less_than(value_1,
-                              ::internal::NumberType<adouble>::value(value_2));
+    return value_is_less_than(
+      value_1, dealii::internal::NumberType<adouble>::value(value_2));
   }
 
 